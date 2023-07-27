a331c0edcb23e6bb3b922dace371c5e633c5e484
sethu <sethu@jananisakthivel>
2023-07-27 11:26:32 +0530

    First

---
+++ C2TC/src/basics_day1/Basics.java
@@ -0,0 +1,15 @@
+package basics_day1;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=5;
+		System.out.println("num1+num2:"+(num1+num2));
+		System.out.println("num1-num2:"+(num1-num2));
+		System.out.println("num1*num2:"+(num1*num2));
+		System.out.println("num1/num2:"+(num1/num2));
+		System.out.println("num1%num2:"+(num1%num2));
+	}
+
+}
